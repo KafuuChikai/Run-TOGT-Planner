b629ad0f83080c0a26aa2980abe8c70950c02665
YamakenElf <810658920qq@gmail.com>
2025-02-27 16:04:59 +0800

    update gate_test.py

--- test_utils/gate_test.py
+++ test_utils/gate_test.py
@@ -57,28 +57,10 @@ hex_kwargs = {
 hex_gate_no_name = create_gate(HexagonPrisma, [0.0, 0.0, 0.0], True, hex_kwargs)
 hex_gate = create_gate(HexagonPrisma, [0.0, 18.0, 0.0], True, hex_kwargs, 'hex_gate')
 
-# print(ball_gate_no_name.to_dict())
-# print(ball_gate.to_dict())
-# print(tri_gate_no_name.to_dict())
-# print(tri_gate.to_dict())
-# print(rec_gate_no_name.to_dict())
-# print(rec_gate.to_dict())
-# print(pen_gate_no_name.to_dict())
-# print(pen_gate.to_dict())
-# print(hex_gate_no_name.to_dict())
-# print(hex_gate.to_dict())
-
 state_kwargs = {
     'pos' : [0.0, 0.0, 0.0],
-    # 'vel' : [1.0, 0.0, 0.0],
-    # 'acc' : [2.0, 0.0, 0.0],
-    # 'jer' : [3.0, 0.0, 0.0],
-    # 'rot' : [4.0, 1.0 ,1.0],
-    # 'cthrustmass' : 5.0,
-    # 'euler' : [6.0, 1.0 ,1.0]
 }
 init_state = create_state(state_kwargs)
-# print(state.to_dict())
 end_state = create_state(state_kwargs)
 
 test_race = RaceTrack(init_state=init_state,
@@ -90,15 +72,12 @@ test_race.add_gate(rec_gate)
 test_race.add_gate(pen_gate)
 test_race.add_gate(hex_gate)
 
-# print(test_race.to_dict())
-# test_race.save_to_yaml(save_dir="track", overwrite=True, standard=False)
+ROOTPATH = os.path.abspath(__file__).split("Run-TOGT-Planner/", 1)[0]
 
-BASEPATH = os.path.abspath(__file__).split("test_utils/", 1)[0]
-
-test_race.save_to_yaml(save_dir=os.path.join(BASEPATH, "resources/racetrack"),overwrite=True, standard=True, save_output=True)
+test_race.save_to_yaml(save_dir=os.path.join(ROOTPATH, "Run-TOGT-Planner/resources/racetrack"),overwrite=True, standard=True, save_output=True)
 read_race = RaceTrack(init_state=init_state,
                       end_state=end_state,
                       race_name='test')
-read_race.load_from_yaml(load_dir=os.path.join(BASEPATH, "../resources/racetrack/race_uzh_7g_multiprisma.yaml"))
+read_race.load_from_yaml(load_dir=os.path.join(ROOTPATH, "Run-TOGT-Planner/resources/racetrack/test.yaml"))
 print(read_race.to_dict())
 read_race.save_to_yaml(overwrite=True, standard=True, save_output=True)
